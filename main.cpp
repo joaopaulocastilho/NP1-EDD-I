6e1f8da0a39f3a6cfdfe2d59a82e2093ab6e571d
Igor Lemos Vicente <igor94@gmail.com>
2015-09-16 18:56:49 -0300

    Versão 0.0

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+Igor Lemos Vicente - igor94@gmail.com
+João Paulo Castilho - 
+ */
+#include <stdio.h>
+#include <math.h>
+
+#define MAX_VALUE 2e31-1
+
+typedef struct Node_t {
+  int value;
+  Node_t *next, *back;
+} node_t;
+
+typedef struct {
+  node_t *begin, *end;
+} list_t;
+
+bool PositiveUnion[MAX_VALUE], NegativeUnion[MAX_VALUE];
+bool PositiveIntersection[MAX_VALUE], NegativeIntersection[MAX_VALUE];
+bool PositiveDistinction[MAX_VALUE], NegativeDistinction[MAX_VALUE];
